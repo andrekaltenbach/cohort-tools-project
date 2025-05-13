57be072771ca80868c7b9c26e1efd4de75e6fb6b
Pedram Ghane <p.ghane@gmail.com>
2025-05-13 17:41:36 +0200

    add Students Model

---
+++ server/models/Students.model.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const studentsSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  linkedinUrl: { type: String, default: "" },
+  languages: {
+    type: [String],
+    enum: [
+      "English",
+      "Spanish",
+      "French",
+      "German",
+      "Portuguese",
+      "Dutch",
+      "Other",
+    ],
+    required: true,
+  },
+  program: {
+    type: String,
+    enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"],
+  },
+  background: { type: String, default: "" },
+  image: { type: String, default: "https://i.imgur.com/r8bo8u7.png" },
+  cohort: { type: Object },
+  projects: { type: Array },
+});
+
+const Students = mongoose.model("Students", studentsSchema);
+
+module.exports = Students;
